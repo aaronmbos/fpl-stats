7e981e6fcdb898f04eed741a2256fbb9c041a4da
Aaron Bos <aaronmbos@gmail.com>
2023-12-28 19:47:48 -0600

    Remove new relic

--- src/aggregator/app.py
+++ src/aggregator/app.py
@@ -1,17 +1,11 @@
-import newrelic.agent
-
-newrelic.agent.initialize()
 import asyncio
 from scraper import scrape
 import logging.config
-from env_util import get_env
 
 
 async def main():
-    newrelic.agent.register_application(timeout=10.0)
-    if get_env() != "production":
-        logging.config.fileConfig("./logging_config/dev.ini")
-        logger.info("Using dev logging configuration")
+    logging.config.fileConfig("./logging_config/dev.ini")
+    logger.info("Using dev logging configuration")
 
     logger = logging.getLogger(__name__)
     try:
@@ -20,8 +14,6 @@ async def main():
         logger.info("Data aggregation completed.")
     except:
         logger.error("An error occurred during data aggregation.", exc_info=True)
-    finally:
-        newrelic.agent.shutdown_agent(timeout=10)
 
 
 asyncio.run(main())
