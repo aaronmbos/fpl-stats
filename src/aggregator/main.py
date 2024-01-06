1a9ade1dd87dae49a1406ac850ab56ba590b866d
Aaron Bos <aaronmbos@gmail.com>
2024-01-06 06:24:58 -0600

    Remove pub/sub code

--- src/aggregator/main.py
+++ src/aggregator/main.py
@@ -10,25 +10,6 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def index():
     try:
-        logger.info("Parsing message.")
-
-        envelope = request.get_json()
-        if not envelope:
-            msg = "No Pub/Sub message received"
-            logger.error(f"Data aggregation skipped. {msg}")
-            return f"Bad Request: {msg}", 204
-
-        if (
-            not isinstance(envelope, dict)
-            or "message" not in envelope
-            or "data" not in envelope["message"]
-            or not base64.b64decode(envelope["message"]["data"]).decode("utf-8").strip()
-            == "scrape"
-        ):
-            msg = "Invalid Pub/Sub message format"
-            logger.error(f"Data aggregation skipped. {msg}")
-            return f"Bad Request: {msg}", 204
-
         logger.info("Starting data aggregation.")
         scrape()
         logger.info("Data aggregation completed.")
